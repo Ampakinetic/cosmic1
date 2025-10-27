16ce8e0362aac4596bdbd5390e8b64441c7e3d55
Mike <admin@ediy.nz>
2025-10-28 10:28:20 +1300

    Ignore some files

--- include/wifi_config.h
+++ include/wifi_config.h
@@ -2,7 +2,7 @@
 #define WIFI_CONFIG_H
 
 // WiFi credentials - replace with your actual WiFi network details
-const char *WIFI_SSID = "**********";
+const char *WIFI_SSID = "*";
 const char *WIFI_PASSWORD = "**********";
 
-#endif // WIFI_CONFIG_H
+#endif // WIFI_CONFIG_H 
